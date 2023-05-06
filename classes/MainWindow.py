35bc986843d105cfab35b1ccb39ef78c5c284d27
Jose Fernando <jose.f72000@gmail.com>
2023-05-05 19:53:04 -0500

    Interface

--- classes/MainWindow.py
+++ classes/MainWindow.py
@@ -1,4 +1,5 @@
-from PyQt5.QtWidgets import QMainWindow, QWidget, QGridLayout
+from PyQt5.QtWidgets import QMainWindow, QWidget, QGridLayout, QVBoxLayout, QLabel, QRadioButton, QPushButton, QHBoxLayout, QSizePolicy
+
 from utils.matrix_reading import *
 from utils.matrix_route import MATRIZ_INTERFAZ
 from classes.Box import Box
@@ -8,14 +9,17 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("IA Project")
-        self.setGeometry(0, 0, 600, 600)
+        self.setGeometry(0, 0, 750, 600)
         self.show()
+
+        # Layout principal (horizontal)
+        main_layout = QHBoxLayout()
+
+        # Widget para el panel izquierdo (cuadrícula de imágenes)
+        grid = QGridLayout()
         mr = MatrixReading()
         mr.read(matrix_path=MATRIZ_INTERFAZ)
         self.matriz = mr.matrix
-
-        # Creamos un layout en cuadrícula
-        grid = QGridLayout()
 
         for i in range(len(self.matriz)):
             for j in range(len(self.matriz[i])):
@@ -32,8 +36,42 @@ class MainWindow(QMainWindow):
                 else:
                     grid.addWidget(Box("white", ""), i, j)
 
-        # Creamos el widget dummy y le asignamos el layout horizontal
-        widget = QWidget()
-        widget.setLayout(grid)
+        # Creamos el widget para el panel izquierdo
+        left_panel = QWidget()
+        left_panel.setGeometry(0, 0, 600, 600)
+        left_panel.setLayout(grid)
 
-        self.setCentralWidget(widget)
+        # Widget para el panel derecho (opciones y botón)
+        right_panel = QWidget()
+        right_panel.setGeometry(600, 0, 200, 600)
+        right_layout = QVBoxLayout()
+        right_panel.setLayout(right_layout)
+
+        # Opciones para el algoritmo
+        #options_label = QLabel("Seleccione un algoritmo:")
+        depth_first = QRadioButton("Profundidad")
+        breadth_first = QRadioButton("Amplitud")
+        uniform_cost = QRadioButton("Costo uniforme")
+
+        # Botón de aceptar
+        accept_button = QPushButton("Aceptar")
+
+        # Añadimos los widgets al layout del panel derecho
+        #right_layout.addWidget(options_label)
+        right_layout.addWidget(depth_first)
+        right_layout.addWidget(breadth_first)
+        right_layout.addWidget(uniform_cost)
+        right_layout.addWidget(accept_button)
+
+        # Añadimos los paneles al layout principal
+        main_layout.addWidget(left_panel)
+        main_layout.addWidget(right_panel)
+
+        left_panel.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Preferred)
+
+        # Widget principal
+        main_widget = QWidget()
+        main_widget.setLayout(main_layout)
+
+        # Asignamos el widget principal como central
+        self.setCentralWidget(main_widget)
--- main.py
+++ main.py
@@ -6,4 +6,5 @@ import sys
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
+    window.show()
     sys.exit(app.exec_())
